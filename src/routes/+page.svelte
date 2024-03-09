3cce92c01246a646313b5a57ffad2634b372d0e5
ersonp <ersonpereiracr7@gmail.com>
2024-03-09 12:50:43 +0530

    feat: add login route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,9 +19,7 @@
 		</figure>
 		<!-- / -->
 		<div class="flex justify-center space-x-2">
-			<a class="btn variant-filled" href="https://skeleton.dev/" target="_blank" rel="noreferrer">
-				Launch Documentation
-			</a>
+			<a class="btn variant-filled" href="/login"> Launch Documentation </a>
 		</div>
 		<div class="space-y-2">
 			<p>Try editing the following:</p>
