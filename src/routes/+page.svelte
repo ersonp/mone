a4fb9f920528c6483fc715dc5407380e8d89df44
ersonp <ersonpereiracr7@gmail.com>
2024-03-09 21:01:43 +0530

    fix(+page.svelte): update heading text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@
 <div class="lg:p-8 w-full">
 	<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
 		<div class="flex flex-col space-y-2 text-center">
-			<h1 class="text-2xl font-semibold tracking-tight">Choose Backend Type</h1>
+			<h1 class="text-2xl font-semibold tracking-tight">Mone</h1>
 			<p class="text-sm text-muted-foreground"></p>
 		</div>
 		<ChooseBackend />
