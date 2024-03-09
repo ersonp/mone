7ab0b2abd92b29377dd8a3e1c0bfc6abcc1e169f
ersonp <ersonpereiracr7@gmail.com>
2024-03-09 17:42:34 +0530

    build: add shadcn-svelte card, select and radio-group

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
+
+	import * as Card from '$lib/components/ui/card';
 	import UserAuthForm from './(components)/user-auth-form.svelte';
 </script>
 
