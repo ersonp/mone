8575ccb2191d710e279e6f612047757a21fe7c18
ersonp <ersonpereiracr7@gmail.com>
2024-03-11 18:17:50 +0530

    feat: add bottom buttons

--- src/routes/(components)/sidebar.svelte
+++ src/routes/(components)/sidebar.svelte
@@ -4,6 +4,8 @@
 	import Moon from 'svelte-radix/Moon.svelte';
 	import ChevronLeftProps from 'svelte-radix/ChevronLeft.svelte';
 	import ChevronRightProps from 'svelte-radix/ChevronRight.svelte';
+	import Avatar from 'svelte-radix/Avatar.svelte';
+	import Gear from 'svelte-radix/Gear.svelte';
 	import { Button } from '$lib/components/ui/button';
 	import { toggleMode } from 'mode-watcher';
 	let isOpen = true;
@@ -27,7 +29,7 @@
 					{:else}
 						<ChevronRightProps class="absolute h-[1.2rem] w-[1.2rem]" />
 					{/if}
-					<span class="sr-only">Toggle theme</span>
+					<span class="sr-only">Toggle sideabar</span>
 				</Button>
 			</div>
 			<ul class="flex flex-col overflow-hidden space-y-2 px-2">
@@ -92,16 +94,34 @@
 					</a>
 				</li>
 			</ul>
-			<ul class="flex flex-col overflow-hidden space-y-2 px-2">
+			<ul
+				class="{isOpen
+					? 'flex-row space-x-2 justify-between'
+					: 'flex-col space-y-2'} flex overflow-hidden px-2"
+			>
+				<div class={isOpen ? 'flex flex-row space-x-2' : 'space-y-2'}>
+					<li class="flex items-center">
+						<Button on:click={toggleMode} variant="outline" size="icon">
+							<Sun
+								class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+							/>
+							<Moon
+								class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+							/>
+							<span class="sr-only">Toggle theme</span>
+						</Button>
+					</li>
+					<li class="flex items-center">
+						<Button variant="outline" size="icon">
+							<Gear class="h-[1.2rem] w-[1.2rem]" />
+							<span class="sr-only">Settings</span>
+						</Button>
+					</li>
+				</div>
 				<li class="flex items-center">
-					<Button on:click={toggleMode} variant="outline" size="icon">
-						<Sun
-							class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-						/>
-						<Moon
-							class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-						/>
-						<span class="sr-only">Toggle theme</span>
+					<Button variant="outline" size="icon">
+						<Avatar class="h-[1.2rem] w-[1.2rem]" />
+						<span class="sr-only">Profile</span>
 					</Button>
 				</li>
 			</ul>
