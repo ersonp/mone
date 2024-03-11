c97f950ce5841905e48d799604149278b951f3e2
ersonp <ersonpereiracr7@gmail.com>
2024-03-11 22:29:25 +0530

    fix(sidebar-top): fix the <a> tag

--- src/routes/(components)/sidebar-top.svelte
+++ src/routes/(components)/sidebar-top.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { writable } from 'svelte/store';
 	import PieChart from 'svelte-radix/PieChart.svelte';
 	import SketchLogo from 'svelte-radix/SketchLogo.svelte';
 	import Input from 'svelte-radix/Input.svelte';
@@ -9,43 +8,43 @@
 </script>
 
 <ul class="flex flex-col overflow-hidden space-y-2 px-2">
-	<li
-		class="{isOpen
-			? undefined
-			: 'items-center justify-center'} flex p-2 hover:bg-primary-foreground hover:rounded {$page.url
-			.pathname === '/savings'
-			? 'bg-primary-foreground rounded'
-			: undefined}"
-	>
-		<a href="/savings" class="flex">
+	<a href="/savings">
+		<li
+			class="{isOpen
+				? undefined
+				: 'items-center justify-center'} flex p-2 hover:bg-primary-foreground hover:rounded {$page
+				.url.pathname === '/savings'
+				? 'bg-primary-foreground rounded'
+				: undefined}"
+		>
 			<SketchLogo />
 			<span class={isOpen ? 'pl-4' : 'hidden'}> Savings </span>
-		</a>
-	</li>
-	<li
-		class="{isOpen
-			? undefined
-			: 'items-center justify-center'} flex p-2 hover:bg-primary-foreground hover:rounded {$page.url
-			.pathname === '/accounts'
-			? 'bg-primary-foreground rounded'
-			: undefined}"
-	>
-		<a href="/accounts" class="flex">
+		</li>
+	</a>
+	<a href="/accounts">
+		<li
+			class="{isOpen
+				? undefined
+				: 'items-center justify-center'} flex p-2 hover:bg-primary-foreground hover:rounded {$page
+				.url.pathname === '/accounts'
+				? 'bg-primary-foreground rounded'
+				: undefined}"
+		>
 			<Input />
 			<span class={isOpen ? 'pl-4' : 'hidden'}> Accounts </span>
-		</a>
-	</li>
-	<li
-		class="{isOpen
-			? undefined
-			: 'items-center justify-center'} flex p-2 hover:bg-primary-foreground hover:rounded {$page.url
-			.pathname === '/insights'
-			? 'bg-primary-foreground rounded'
-			: undefined}"
-	>
-		<a href="/insights" class="flex">
+		</li>
+	</a>
+	<a href="/insights">
+		<li
+			class="{isOpen
+				? undefined
+				: 'items-center justify-center'} flex p-2 hover:bg-primary-foreground hover:rounded {$page
+				.url.pathname === '/insights'
+				? 'bg-primary-foreground rounded'
+				: undefined}"
+		>
 			<PieChart />
 			<span class={isOpen ? 'pl-4' : 'hidden'}> Insights </span>
-		</a>
-	</li>
+		</li>
+	</a>
 </ul>
