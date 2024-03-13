d90f2471e52c2bab0d36f783c54023078ffd1989
ersonp <ersonpereiracr7@gmail.com>
2024-03-14 01:10:46 +0530

    reafactor(toolbar): add button

--- src/routes/savings/(components)/data-table-toolbar.svelte
+++ src/routes/savings/(components)/data-table-toolbar.svelte
@@ -5,6 +5,7 @@
 	import type { TableViewModel } from 'svelte-headless-table';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import Cross2 from 'svelte-radix/Cross2.svelte';
+	import CardStackPlus from 'svelte-radix/CardStackPlus.svelte';
 	import { invTypes, statuses, names } from '../(data)/data.js';
 	import type { Writable } from 'svelte/store';
 
@@ -36,35 +37,43 @@
 			<Input placeholder="Filter..." class="h-8 " type="search" bind:value={$filterValue} />
 		</div>
 	</div>
-	<div class="flex flex-wrap">
-		<DataTableFacetedFilter
-			bind:filterValues={$filterValues.invType}
-			title="Inv Type"
-			options={invTypes}
-		/>
-		<DataTableFacetedFilter
-			bind:filterValues={$filterValues.status}
-			title="Inv Status"
-			options={statuses}
-		/>
-		<DataTableFacetedFilter bind:filterValues={$filterValues.name} title="Name" options={names} />
-		{#if showReset}
-			<div class="pt-1">
-				<Button
-					on:click={() => {
-						$filterValue = '';
-						$filterValues.invType = [];
-						$filterValues.status = [];
-						$filterValues.name = [];
-					}}
-					variant="ghost"
-					class="h-8 px-2 lg:px-3"
-				>
-					Reset
-					<Cross2 class="ml-2 h-4 w-4" />
-				</Button>
-			</div>
-		{/if}
+	<div class="flex flex-wrap justify-between">
+		<div class="flex flex-wrap">
+			<DataTableFacetedFilter
+				bind:filterValues={$filterValues.invType}
+				title="Inv Type"
+				options={invTypes}
+			/>
+			<DataTableFacetedFilter
+				bind:filterValues={$filterValues.status}
+				title="Inv Status"
+				options={statuses}
+			/>
+			<DataTableFacetedFilter bind:filterValues={$filterValues.name} title="Name" options={names} />
+			{#if showReset}
+				<div class="pt-1">
+					<Button
+						on:click={() => {
+							$filterValue = '';
+							$filterValues.invType = [];
+							$filterValues.status = [];
+							$filterValues.name = [];
+						}}
+						variant="ghost"
+						class="h-8 px-2 lg:px-3"
+					>
+						Reset
+						<Cross2 class="ml-2 h-4 w-4" />
+					</Button>
+				</div>
+			{/if}
+		</div>
+		<div class="pt-1">
+			<Button variant="outline" class="h-8 bg-primary text-secondary">
+				Add
+				<CardStackPlus class="ml-2 h-4 w-4" />
+			</Button>
+		</div>
 	</div>
 
 	<DataTableViewOptions {tableModel} />
