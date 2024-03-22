736b6372535480032a53d079b79e7026e8782e9b
ersonp <ersonpereiracr7@gmail.com>
2024-03-23 02:35:45 +0530

    feat(savings-form): add all form fields

--- src/routes/savings/(components)/savings-form.svelte
+++ src/routes/savings/(components)/savings-form.svelte
@@ -3,24 +3,14 @@
 	import { investmentSchema, type InvestmentSchema } from '../(data)/schemas';
 	import SuperDebug, { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
 	import { zodClient } from 'sveltekit-superforms/adapters';
-	import { browser } from '$app/environment';
-	import { page } from '$app/stores';
 	import CalendarIcon from 'svelte-radix/Calendar.svelte';
-	import { cn } from '$lib/utils.js';
-	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
-	import { Calendar } from '$lib/components/ui/calendar/index.js';
-	import * as Popover from '$lib/components/ui/popover/index.js';
-	import * as Form from '$lib/components/ui/form/index.js';
+	import * as Select from '$lib/components/ui/select';
+	import * as Form from '$lib/components/ui/form';
+	import { invTypes, returnTypes, statuses, names } from '../(data)/data.js';
 	// import { toast } from 'svelte-sonner';
 	import { DatePicker } from '.';
-	import {
-		type DateValue,
-		DateFormatter,
-		getLocalTimeZone,
-		parseDate,
-		CalendarDate,
-		today
-	} from '@internationalized/date';
+	import { type DateValue, DateFormatter } from '@internationalized/date';
+	import Dropdown from '../../(components)/dropdown.svelte';
 	export let data: SuperValidated<Infer<InvestmentSchema>>;
 
 	const form = superForm(data, {
@@ -31,41 +21,110 @@
 	const df = new DateFormatter('en-US', {
 		dateStyle: 'long'
 	});
-	let placeholder: DateValue = today(getLocalTimeZone());
 	const { form: formData, enhance } = form;
-	let value: DateValue | undefined = undefined;
+
 	let startDateValue: DateValue | undefined = undefined;
 	let endDateValue: DateValue | undefined = undefined;
-	$: value = $formData.start_date ? parseDate($formData.start_date) : undefined;
 </script>
 
 <form method="POST" use:enhance>
+	<!-- Start Date -->
 	<Form.Field {form} name="start_date">
 		<Form.Control let:attrs>
 			<Form.Label>Start Date</Form.Label>
-			<DatePicker attrs value={endDateValue} formValue={'start_date'} bind:formData={$formData} />
-			<Form.Description>When did you start this investment?</Form.Description>
+			<DatePicker attrs formValue={'start_date'} bind:formData={$formData} />
 			<input hidden value={$formData.start_date} name={attrs.name} />
-			<Form.FieldErrors />
 		</Form.Control>
+		<Form.Description>When did you start this investment?</Form.Description>
+		<Form.FieldErrors />
 	</Form.Field>
+
+	<!-- End Date -->
 	<Form.Field {form} name="end_date">
 		<Form.Control let:attrs>
 			<Form.Label>End Date</Form.Label>
-			<DatePicker attrs value={endDateValue} formValue={'end_date'} bind:formData={$formData} />
-			<Form.Description>When will this investment end?</Form.Description>
+			<DatePicker attrs formValue={'end_date'} bind:formData={$formData} minDate={'start_date'} />
 			<input hidden value={$formData.end_date} name={attrs.name} />
-			<Form.FieldErrors />
 		</Form.Control>
+		<Form.Description>When will this investment end?</Form.Description>
+		<Form.FieldErrors />
 	</Form.Field>
+
+	<!-- Inv Name -->
+	<Form.Field {form} name="inv_name">
+		<Form.Control let:attrs>
+			<Form.Label>Inv Name</Form.Label>
+			<Input {...attrs} bind:value={$formData.inv_name} />
+		</Form.Control>
+		<Form.Description>The name of the investment.</Form.Description>
+		<Form.FieldErrors />
+	</Form.Field>
+
+	<!-- Investment Type -->
+	<Form.Field {form} name="inv_type">
+		<Form.Control let:attrs>
+			<Form.Label>Investment Type</Form.Label>
+			<Dropdown bind:formData={$formData} options={invTypes} formValue="inv_type" {attrs} />
+			<input hidden bind:value={$formData.inv_type} name={attrs.name} />
+		</Form.Control>
+		<Form.Description>The type of investment this is.</Form.Description>
+		<Form.FieldErrors />
+	</Form.Field>
+
+	<!-- Return Type -->
+	<Form.Field {form} name="return_type">
+		<Form.Control let:attrs>
+			<Form.Label>Return Type</Form.Label>
+			<Dropdown bind:formData={$formData} options={returnTypes} formValue="return_type" {attrs} />
+			<input hidden bind:value={$formData.return_type} name={attrs.name} />
+		</Form.Control>
+		<Form.Description>The type of return this investment will have.</Form.Description>
+		<Form.FieldErrors />
+	</Form.Field>
+
+	<!-- Return Rate -->
+	<Form.Field {form} name="return_rate">
+		<Form.Control let:attrs>
+			<Form.Label>Return Rate</Form.Label>
+			<Input {...attrs} bind:value={$formData.return_rate} type="number" />
+		</Form.Control>
+		<Form.Description>The rate of return for this investment.</Form.Description>
+		<Form.FieldErrors />
+	</Form.Field>
+
+	<!-- Investment Amount -->
+	<Form.Field {form} name="inv_amount">
+		<Form.Control let:attrs>
+			<Form.Label>Investment Amount</Form.Label>
+			<Input {...attrs} bind:value={$formData.inv_amount} type="number" />
+		</Form.Control>
+		<Form.Description>The amount of money invested.</Form.Description>
+		<Form.FieldErrors />
+	</Form.Field>
+
+	<!-- Return Amount -->
+	<Form.Field {form} name="return_amount">
+		<Form.Control let:attrs>
+			<Form.Label>Return Amount</Form.Label>
+			<Input {...attrs} bind:value={$formData.return_amount} type="number" />
+		</Form.Control>
+		<Form.Description>The amount of promised return.</Form.Description>
+		<Form.FieldErrors />
+	</Form.Field>
+
+	<!-- Name -->
 	<Form.Field {form} name="name">
 		<Form.Control let:attrs>
 			<Form.Label>Name</Form.Label>
-			<Input {...attrs} bind:value={$formData.name} />
+			<Dropdown bind:formData={$formData} options={names} formValue="name" {attrs} />
+			<input hidden bind:value={$formData.name} name={attrs.name} />
 		</Form.Control>
 		<Form.Description>The name of the person this investment is on.</Form.Description>
 		<Form.FieldErrors />
 	</Form.Field>
-	<Form.Button>Submit</Form.Button>
+
+	<div class="pt-2">
+		<Form.Button>Submit</Form.Button>
+	</div>
 	<SuperDebug data={$formData} />
 </form>
