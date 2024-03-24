a4b3b22b035d37c6eead16863e681f65dbd7e490
ersonp <ersonpereiracr7@gmail.com>
2024-03-24 21:53:51 +0530

    fix(schemas): form number validation

--- src/routes/investments/(components)/inv-form.svelte
+++ src/routes/investments/(components)/inv-form.svelte
@@ -16,11 +16,6 @@
 	});
 
 	const { form: formData, enhance } = form;
-
-	// TODO(ersonp): move this to submit handler
-	// $: $formData.return_rate = Number($formData.return_rate);
-	// $: $formData.inv_amount = Number($formData.inv_amount);
-	// $: $formData.return_amount = Number($formData.return_amount);
 </script>
 
 <form method="POST" use:enhance>
